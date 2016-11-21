8d71a04226266dd89afa3281f8f34cad629af32d
alincode <alincode@gmail.com>
2016-11-21 12:13:21 +0800

    add one more sample

--- test/lib.product.js
+++ test/lib.product.js
@@ -88,4 +88,23 @@ describe('product page', function() {
       done(e);
     }
   });
+
+  it('case 4', async(done) => {
+    try {
+      let html = await getHtml(
+        'sample4.html'
+      );
+      let digikey = new Digikey(html,
+        'http://www.digikey.com.cn/search/zh/LM358ADT/497-1590-1-ND?recordId=592082'
+      );
+      let result = await digikey.getResult();
+      result.should.have.keys(ProductFullFields);
+      checklist(result);
+      result.documents.should.be.a('array');
+      result.documents.length.should.above(0);
+      done();
+    } catch (e) {
+      done(e);
+    }
+  });
 });
